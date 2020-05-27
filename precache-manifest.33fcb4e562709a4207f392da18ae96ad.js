636edd4d4f959fde6d00d1b07cd9cd54ea3e9ea4
Greg2010iMac <gregorio.jp.mitchell@gmail.com>
2020-05-27 11:50:14 -0700

    Updates

---
+++ precache-manifest.33fcb4e562709a4207f392da18ae96ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e271628df456963ff5e3f7c5e31c15",
+    "url": "/test00/index.html"
+  },
+  {
+    "revision": "1578415a7631bba69f51",
+    "url": "/test00/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fa0919225d08fd3feb6",
+    "url": "/test00/static/js/2.a12478d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test00/static/js/2.a12478d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1578415a7631bba69f51",
+    "url": "/test00/static/js/main.4cc00eb1.chunk.js"
+  },
+  {
+    "revision": "4332437a0ae3d7725bc2",
+    "url": "/test00/static/js/runtime-main.4bb996ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test00/static/media/logo.5d5d9eef.svg"
+  }
+]);
